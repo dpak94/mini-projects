f58af827f71d91e29d2dc40e0055c8649f0bf017
Deepak Mitta <109253936+dpak94@users.noreply.github.com>
2023-08-10 18:39:04 +0530

    Update quiz.py

---
+++ quiz-game/src/quiz.py
@@ -0,0 +1,9 @@
+# A Basic Geography Quiz Program
+
+print("Hello and welcome to Geography Quiz")
+play = input("Would youu like to play the quiz? ").lower()
+
+if play not in ['yes', 'y', 1]:
+    quit()
+
+score = 0
