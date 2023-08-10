d1ff2026cbbb600bf894895ecd4b7a868dc117c0
Deepak Mitta <109253936+dpak94@users.noreply.github.com>
2023-08-10 21:32:21 +0530

    add quiz2.py

---
+++ quiz-game-2/src/quiz2.py
@@ -0,0 +1,27 @@
+''' In this program, instead of having a fixed set of questions and 
+answers entered within the code, nations and their capitals are 
+sourced from a dataset using Pandas. Using random functions, a random nation and 
+its capital will be chosen for the quiz.'''
+
+import pandas as pd
+import random, time, os
+
+# Setting the Working Directory
+os.chdir(r'quiz-game-2/src')
+# print(os.getcwd())
+
+# Pleasantries
+print("Welcome to Geopolitical Quiz!!!")
+print("Would you like to participate in this quiz? ")
+play = input().lower()
+
+if play not in ['yes', 'y', '1']:
+    quit()
+
+# Sourcing data from given dataset - country-list.csv
+df = pd.read_csv("./data/country-list.csv")
+
+print(df.columns)
+# 5 questions were given in this quiz
+
+print(df.sample())
