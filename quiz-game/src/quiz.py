9db757554c77b665ee9061a1de0bf39ddbb3f857
Deepak Mitta <109253936+dpak94@users.noreply.github.com>
2023-08-10 18:57:45 +0530

    Update quiz.py

--- quiz-game/src/quiz.py
+++ quiz-game/src/quiz.py
@@ -1,9 +1,50 @@
 # A Basic Geography Quiz Program
 
-print("Hello and welcome to Geography Quiz")
-play = input("Would youu like to play the quiz? ").lower()
+# Importing time module
+import time
 
-if play not in ['yes', 'y', 1]:
+print("Hello and welcome to Geography Quiz")
+play = input("Would you like to play the quiz? ").lower()
+
+
+if play not in ['yes', 'y', '1']:
     quit()
 
 score = 0
+
+print("Alright, here's the first question :")
+
+# Q&A Section
+answer = input("Name the capital of Australia? ")
+if answer.lower() == "canberra":
+    score += 1
+    print("Correct! Next question :")
+else:
+    print("Wrong! Next question :")
+
+time.sleep(2)
+
+answer = input("On which continent is River Nile located? ")
+if answer.lower() == "africa":
+    print("Correct! Next question :")
+    score += 1
+else:
+    print("Wrong! Next question :")
+
+time.sleep(2)
+
+answer = input("Name the most populous country in the world ? ")
+if answer.lower() == "india":
+    print("Correct!")
+    score += 1
+else:
+    print("Wrong! ")
+
+time.sleep(2)
+
+# Final Score printout
+print("Your total score is :", score)
+if score < 3:
+    print("Better luck next time.")
+else:
+    print("Well Played!")
